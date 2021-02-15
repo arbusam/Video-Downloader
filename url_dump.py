a0cbcfa53979641212ebe6a9a956c975c10b375a
Arhan Busam <arhan.busam@gmail.com>
2021-02-15 16:38:57 +1100

    Deleted comments

--- url_dump.py
+++ url_dump.py
@@ -1,14 +1,3 @@
-# Calling this file will add the URL
-# into the 'url_dump' table.
-
-# Homework Tasks
-# 1. Create a table 'url_dump' if not exists.
-# 2. Accept the URL from the user.
-# 3. Finally, add that into the 'url_dump' table.
-# Optional
-# 4. Add the ability to provide a filename containing
-#    a list of URLs (one URL per line).
-
 from db import create_table, add_data, file_read
 
 table_name = "url_dump"
