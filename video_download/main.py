8d0b2ae42d547f889db3021b595495fca411e649
Arhan Busam <arhan.busam@gmail.com>
2021-02-26 18:37:58 +1100

    Poetry add error

---
+++ video_download/main.py
@@ -0,0 +1,4 @@
+from __future__ import unicode_literals
+import youtube_dl
+import colorama
+from os import expanduser, dirname, realpath
---
+++ video_downloader/__init__.py
@@ -0,0 +1,1 @@
+# This is an unused folder required for poetry.
