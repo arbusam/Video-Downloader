cb1185228257cf090e83bbbc5d2ad1f6ac262215
Arhan Busam <arhan.busam@gmail.com>
2021-02-27 17:08:09 +1100

    Added download code

--- video_download/main.py
+++ video_download/main.py
@@ -6,4 +6,15 @@ from os.path import expanduser, dirname, realpath
 # home = expanduser("~")
 # print(home)
 
-print(realpath(__file__))
+filepath = realpath(__file__)
+sub_dir = dirname(realpath(__file__))
+parent_dir = dirname(dirname(realpath(__file__)))
+
+videos_dir = "[videos]"
+file_format_basename = "[%(channel)s]-[%(upload_date)s]-[%(title)s]-[%(id)s]"
+
+links = ["https://www.youtube.com/watch?v=d-diB65scQU"]
+
+ydl_opts = {}
+with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+    ydl.download(links)
