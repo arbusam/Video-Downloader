7ceca3627f004e06f6dcc9b0cf826616acf5ccc7
Arhan Busam <arhan.busam@gmail.com>
2021-03-05 18:08:27 +1100

    Changed video link

--- video_download/main.py
+++ video_download/main.py
@@ -13,7 +13,7 @@ parent_dir = dirname(dirname(realpath(__file__)))
 videos_dir = "[videos]"
 file_format_basename = "[%(channel)s]-[%(upload_date)s]-[%(title)s]-[%(id)s]"
 
-links = ["https://www.youtube.com/watch?v=d-diB65scQU"]
+links = ["https://www.youtube.com/watch?v=BaW_jenozKc"]
 
 ydl_opts = {}
 with youtube_dl.YoutubeDL(ydl_opts) as ydl:
