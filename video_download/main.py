4cd7fec4bbafa705280597b7960e6af1555d79d2
Arhan Busam <arhan.busam@gmail.com>
2021-03-05 18:24:23 +1100

    Changes

--- video_download/main.py
+++ video_download/main.py
@@ -6,15 +6,16 @@ from os.path import expanduser, dirname, realpath
 # home = expanduser("~")
 # print(home)
 
-filepath = realpath(__file__)
-sub_dir = dirname(realpath(__file__))
-parent_dir = dirname(dirname(realpath(__file__)))
+def start():
+    filepath = realpath(__file__)
+    sub_dir = dirname(realpath(__file__))
+    parent_dir = dirname(dirname(realpath(__file__)))
 
-videos_dir = "[videos]"
-file_format_basename = "[%(channel)s]-[%(upload_date)s]-[%(title)s]-[%(id)s]"
+    videos_dir = "[videos]"
+    file_format_basename = "[%(channel)s]-[%(upload_date)s]-[%(title)s]-[%(id)s]"
 
-links = ["https://www.youtube.com/watch?v=d-diB65scQU"]
+    links = ["https://www.youtube.com/watch?v=d-diB65scQU"]
 
-ydl_opts = {}
-with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-    ydl.download(links)
+    ydl_opts = {}
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download(links)
