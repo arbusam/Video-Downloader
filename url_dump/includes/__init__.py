854699c00f735094dfdb10b34b678ae006941664
Arhan Busam <arhan.busam@gmail.com>
2021-03-19 18:37:47 +1100

    Moved imports to __init__.py

---
+++ url_dump/includes/__init__.py
@@ -0,0 +1,5 @@
+from .functions import *
+from .errors import *
+from ..db import create_table, add_data, file_read, read_from_table
+import argparse
+from colorama import init, Fore, Back, Style
--- url_dump/main.py
+++ url_dump/main.py
@@ -1,6 +1,4 @@
-from .db import create_table, add_data, file_read, read_from_table
-import argparse
-from colorama import init, Fore, Back, Style
+from .includes import *
 
 table_name = "url_dump"
 
