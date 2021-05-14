4a04385509c049e4c06b0356a25cc1192d1190b7
Arhan Busam <arhan.busam@gmail.com>
2021-05-14 18:31:49 +1000

    Fixed error formatting code

--- url_dump/includes/errors.py
+++ url_dump/includes/errors.py
@@ -2,5 +2,5 @@ from colorama import init, Fore, Back, Style
 
 URL_FILE_BOTH_PRESENT = Fore.RED + "Either URL or the Filename are allowed. But not both!"
 FILE_NOT_FOUND = Fore.RED + "The file provided does not exist."
-DUPLICATES = Fore.WHITE + f"One or more of the URLs provided are either {Fore.RED}already in the table {Fore.RESET}or {Fore.RED}are duplicates in the file. Those URLs will not be added."
+DUPLICATES = f"{Fore.WHITE}One or more of the URLs provided are either {Fore.RED}already in the table {Fore.RESET}or {Fore.RED}are duplicates in the file. Those URLs will not be added."
 INVALID_TYPE_ERROR = Fore.RED + "Invalid type passed in. Reverting to default types."
